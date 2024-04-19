e8e51d39072ae0e4390c416e9c2f048efb3c17a4
Bryan Orozco <bryanorozcoweb@gmail.com>
2024-04-19 10:27:20 -0700

    geometry setup

---
+++ src/app/components/box.js
@@ -0,0 +1,67 @@
+"use client";
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import * as THREE from "three";
+
+export function Box(props) {
+
+const vertexShader = `
+  varying vec2 vUv;
+
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`;
+
+const fragmentShader = `
+  varying vec2 vUv;
+  uniform vec2 u_resolution;
+  uniform vec2 u_mouse;
+  uniform float u_time;
+  float plot(vec2 st) {    
+    return smoothstep(0.02, 0.0, abs(st.y - st.x));
+  }
+  
+
+  void main() {
+    vec2 st = vUv.xy;
+    float y = st.x;
+
+    vec3 color = vec3(y);
+
+    // Plot a line
+    float pct = plot(st);
+    color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
+
+    gl_FragColor = vec4(color,1.0);
+    
+  }
+`;
+const Box = () => {
+  
+  // Create the Box geometry
+  const geometry = new THREE.BoxGeometry( 5, 5, 1 ); 
+
+  // Create the shader material
+  const material = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+  });
+
+  return (
+    <mesh geometry={geometry} material={material}>
+    </mesh>
+  );
+};
+
+  return (
+    <div className="h5">
+      <Canvas dpr={Math.max(window.devicePixelRatio, 2)}>
+        <Box/>
+      </Canvas>
+    </div>
+
+    
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,26 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Box } from './components/box'
+import 'tachyons/css/tachyons.min.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-washed-blue">
+        <div className="mw9 center ph3-ns">
+          <div className="cf ph2-ns">
+            <div className="fl w-100 w-30-ns pa2">
+              <div className="outline bg-white pv4">
+                <h1 className="gold">Hola</h1>
+                <p>I’m Bryan Orozco, a Software Developer based out of Los Angeles California. </p>
+                <a className="mt3 f6 no-underline br-pill ba ph3 pv2 mb2 dib gold" href="#0">download my resume</a>
+              </div>
+            </div>
+            <div className="fl w-100 w-70-ns pa2">
+              <div className="outline bg-white pv4">
+                <Box />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        
     </main>
   );
 }
