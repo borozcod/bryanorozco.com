47975e9352dc8ca5d5f4271e4621dc49c6620722
Bryan Orozco <bryanorozcoweb@gmail.com>
2024-04-19 11:09:12 -0700

    commented out geometry

--- src/app/page.js
+++ src/app/page.js
@@ -3,20 +3,20 @@ import 'tachyons/css/tachyons.min.css';
 
 export default function Home() {
   return (
-    <main className="bg-washed-blue">
+    <main className="bg-white">
         <div className="mw9 center ph3-ns">
           <div className="cf ph2-ns">
-            <div className="fl w-100 w-30-ns pa2">
+            <div className="fl w-100 w-20-ns pa2">
               <div className="bg-white pv4">
                 <h1 className="gold">Hola</h1>
                 <p>I’m Bryan Orozco, a Software Developer based out of Los Angeles California. </p>
               </div>
             </div>
-            <div className="fl w-100 w-70-ns pa2">
+            {/* <div className="fl w-100 w-70-ns pa2">
               <div className="bg-white pv4">
                 <Box />
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
         
