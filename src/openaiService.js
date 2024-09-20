8214b05523233a0a8180bde380ec5bacc6cdd9e3
Bryan Orozco <bryanorozcoweb@gmail.com>
2024-09-19 19:01:21 -0700

    converted function to server side

--- src/openaiService.js
+++ src/openaiService.js
@@ -1,10 +1,8 @@
+'use server'
 import OpenAI from "openai";
 const openai = new OpenAI({
-    apiKey: "apiKey",
-    dangerouslyAllowBrowser: true
+    apiKey: process.env.OPENAI_API_KEY
 });
-
-console.log(process.env.NEXT_PUBLIC_OPENAI_API_KEY)
 
 export const getDALLEImage = async (prompt) => {
     const image = await openai.images.generate({ 
@@ -14,8 +12,5 @@ export const getDALLEImage = async (prompt) => {
         size: '1024x1024',
         response_format: "b64_json"
     });
-
-    console.log(image.data[0].b64_json);
-
   return image.data[0].b64_json;
 };
